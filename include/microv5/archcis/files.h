d0e444fd5854ce723936af2d15d0f39fecc1bc5f
410 <58301450+410-dev@users.noreply.github.com>
2020-12-29 17:00:05 +0900

    Functions

--- include/microv5/archcis/files.h
+++ include/microv5/archcis/files.h
@@ -1,1 +1,18 @@
 #include "main.h"
+#include <string.h>
+
+std::string fReadStringFromFile(std::string fileName) {
+
+}
+
+short fWriteStringToFile(std::string fileName, std::string fileContent ) {
+
+}
+
+bool fFileExists(std::string fileName) {
+  
+}
+
+short fRenameFile(std::string original, std::string renameTo) {
+
+}
